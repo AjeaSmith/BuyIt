dc0215f1d435a71038c96d02dd3e385a6dbaa3a4
ajeas <smithajea@rocketmail.com>
2020-11-02 03:15:29 -0500

    setup navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
-      Hello to Buy It
+     <Header/>
+        <main className="py-3">
+          <Container>
+              <h1>Welcome to Buy It</h1>
+          </Container>
+        </main>
+     <Footer/>
     </div>
   );
 }
